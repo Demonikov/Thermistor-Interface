0fc79abd8564967fa8692b23c12ac6e54b4a60ad
Demonikov <vincent.dupont99@hotmail.ca>
2022-04-13 14:56:13 -0400

    Design plus propre2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <title>LAB 4</title>
 
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="w3.css">
         <link rel="stylesheet" href="../jqwidgets/jqwidgets/styles/jqx.base.css">
         <link rel="stylesheet" href="default.css">
 
@@ -34,39 +34,29 @@
         </div>
     </nav>
 
-    <body style="margin-left: 10%; margin-right: 10%">
-        <?php
-        require_once 'classThermistor.php';
-
-        $obj1 = new Thermistor();
-        ?>
-
-
+    <body style="margin:0 5%">
         <div id='content'>
-            <form style="width: 550px; float: left">
-
+            <div id=inputSection>
                 <div class="lineContainer">
                     <span>VCC</span>
                     <div class="inputContainer" id='VCC'></div>
                 </div>
-
                 <div class="lineContainer">
                     <span>RDIV</span>
                     <div class="inputContainer" id='RDIV'></div>
                 </div>
-
                 <div class="lineContainer">
                     <span>VADC</span>
                     <div class="inputContainer" id='VADC' style="float: right"></div>
                 </div>
-
                 <div id="UnitList" class="lineContainter">
                     <div class="cUnit" id="UNIT_C">Celsius</div>
                     <div class="cUnit" id="UNIT_F">Fahraneint</div>
                     <div class="cUnit" id="UNIT_K">Kelvin</div>
                 </div>
-            </form>
-            <div id="tempGauge" style="float: left; padding: 10px"></div>
+            </div>
+
+            <div id="tempGauge"></div>
         </div>
     </body>
 
