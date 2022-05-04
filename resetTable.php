9b536fc48270a029390308aa8bcb482e30c780d0
Demonikov <vincent.dupont99@hotmail.ca>
2022-05-03 22:21:11 -0400

    Classe pour requete SQL

--- resetTable.php
+++ resetTable.php
@@ -1,22 +1,6 @@
 <?php
-$db = $_GET['DB'];
 
-$con = new mysqli("localhost", "phpmyadmin", "phpmyadmin", $db);
+include_once('classSQLi.php');
+$tempCON = new SQLi();
 
-if ($con->connect_error)
-	die("Could not connect:" . $con->mysqli_error());
-
-$sql = "DELETE FROM temperature";
-$result = $con->query($sql);
-$sql = "ALTER TABLE temperature AUTO_INCREMENT=1";
-$result = $con->query($sql);
-
-if($result->num_rows > 0){
-	while($row = $result->fetch_assoc()){
-		echo json_encode( array("Temperature" => $row["Temperature"], "Unit" => $row["Unit"], "Time" => $row["Time"]) );
-	}
-}else{
-	echo "No data";
-}
-
-$con->close();
+$tempCON->resetTable("temperature");
